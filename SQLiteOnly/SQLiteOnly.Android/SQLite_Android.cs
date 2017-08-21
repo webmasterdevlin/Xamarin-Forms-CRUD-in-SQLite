def95f3fc1b636fa6e966278d0564d9f3a2ebcfd
webmasterdevlin <webmasterdevlin@gmail.com>
2017-08-21 16:04:21 +0800

    Add project files.

---
+++ SQLiteOnly/SQLiteOnly.Android/SQLite_Android.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using SQLiteOnly.Droid;
+using SQLiteOnly.Persistence;
+using Environment = System.Environment;
+
+[assembly: Dependency(typeof(SQLite_Android))]
+namespace SQLiteOnly.Droid
+{
+    class SQLite_Android : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFileName = "Contacts.db3";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // documents folder
+
+            string libraryPath = Path.Combine(documentsPath); // Library folder
+
+            var path = Path.Combine(libraryPath, sqliteFileName);
+
+            // This is where we copy in the prepopulated database
+
+            if (!File.Exists(path))
+            {
+                File.Copy(sqliteFileName, path);
+            }
+
+            var conn = new SQLite.SQLiteConnection(path);
+
+            // Return the database connection
+            return conn;
+        }
+    }
+}
---
+++ SQLiteOnly/SQLiteOnly.iOS/SQLite_iOS.cs
@@ -0,0 +1,43 @@
+﻿using SQLite;
+using SQLiteOnly.iOS;
+using System;
+using System.IO;
+using SQLiteOnly.Persistence;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_iOS))]
+
+namespace SQLiteOnly.iOS
+{
+    public class SQLite_iOS : ISQLite
+    {
+        public SQLite_iOS()
+        {
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            // variable for sqlite database file
+            var sqliteFileName = "Contacts.db3";
+
+            // variable for special folder
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // documents folder
+
+            string libraryPath = Path.Combine(documentsPath); // library folder
+
+            var path = Path.Combine(libraryPath, sqliteFileName);
+       
+            
+            // This is where we copy the prepopulated database
+            if (!File.Exists(path))
+            {
+                File.Copy(sqliteFileName, path);
+            }
+
+            var conn = new SQLite.SQLiteConnection(path);
+
+            // return the database connection
+            return conn;
+        }
+    }
+}
