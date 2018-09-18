b7504e8471081c1f90b94bb8a83bf75b5c6ec20b
Devlin Duldulao <webmasterdevlin@gmail.com>
2018-09-18 10:20:58 +0800

    Updated recipe

---
+++ SQLiteOnly/SQLiteOnly/Models/Recipe.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SQLite;
+using SQLiteOnly.Annotations;
+
+namespace SQLiteOnly
+{
+    public class Recipe : INotifyPropertyChanged
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        private string _name;
+
+        [MaxLength(255)]
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
